205fb7ec98287fa171b67ddbb06beec59a6a6d7a
RazElbaz <93310416+RazElbaz@users.noreply.github.com>
2022-01-07 15:30:58 +0200

    Update Agent.java

--- src/Agent.java
+++ src/Agent.java
@@ -18,7 +18,7 @@ public class Agent {
     private Pokemon whoIeat;
     private Pokemon pokemon;
 
-
+    //A contracture that initializes the Agent with data
     public Agent(DirectedWeightedGraph g, int node) {
         this.g = g;
         this.node = g.getNode(node);
@@ -29,69 +29,80 @@ public class Agent {
         id = -1;
     }
 
-
+    //Function that returns the id of the Agent
     public int getId() {
         return id;
     }
 
+    //Function that returns the id of the Graph
     public DirectedWeightedGraph getGraph() {
         return this.g;
     }
 
+    //Function that returns the value of the Agent
     public double getValue() {
         return value;
     }
 
+    //Function that change the id of the Agent
     public void setId(int i) {
         id = i;
     }
 
+    //Function that change the value of the Agent
     public void setValue(double val) {
         value = val;
     }
 
-    public void setSpeed(double spe) {
-        speed = spe;
+    //Function that change the speed of the Agent
+    public void setSpeed(double speed) {
+        speed = speed;
     }
 
-    public void setPos(GeoLocation s) {
-        pos = s;
+    //Function that change the pos of the Agent
+    public void setPos(GeoLocation p) {
+        pos = p;
     }
 
+    //Function that return the pos of the Agent
     public GeoLocation getPos() {
         return this.pos;
     }
 
-    public void setWhoIeat(Pokemon curr_fruit) {
-        this.whoIeat = curr_fruit;
+    //Function that return the pokmen that the Agent eaten
+    public void setWhoIeat(Pokemon pok) {
+        this.whoIeat = pok;
     }
 
+    //Function that return the src of the Agent
     public int getSrc() {
         return this.node.getKey();
     }
 
+    //Function that return the dest of the Agent 
     public int getDest() {
-        if (this.edge == null) {
-            return -1;
-        } else {
-            return this.edge.getDest();
-        }
+        if (this.edge == null) { return -1;}
+        else { return this.edge.getDest();}
     }
 
+    //Function that change the dest of the Agent 
     public void setDest(int dest) {
         int src = this.node.getKey();
         this.edge = g.getEdge(src, dest);
         this.dest = dest;
     }
 
+    //Function that change the src of the Agent
     public void setSrc(int src) {
         this.src = src;
     }
 
+    //Function that change pokemon
     public void setPokemon(Pokemon pok) {
         this.pokemon = pok;
     }
 
+    //Function that return if the agent is on node and not moved
     public boolean on_node() {
         Iterator<NodeData> it = g.nodeIter();
         while ((it.hasNext())) {
@@ -121,7 +132,7 @@ public class Agent {
         if(cnt != null){
             cnt_list.add(this.pokemon);
         }
-
+        //loop on the all the Pokemons
         for (int i = 0; i <sizeList; i++) {
             current = cnt_list.get(i).getedge().getSrc();
             double theGrade = cnt_list.get(i).getValue();
@@ -148,11 +159,13 @@ public class Agent {
             //If there is more than one Pokemon on Edge
             for (int j = 0; j< pokemons.size(); j++) {
                 if (pokemons.get(j).equals(cnt_list.get(i))==false) {
+                    //creating variables for the src and dest
                     int srcPokemons=pokemons.get(j).getedge().getSrc();
                     int srcList =cnt_list.get(i).getedge().getSrc();
                     int destPokemons=pokemons.get(j).getedge().getDest();
                     int destList =cnt_list.get(i).getedge().getDest();
                     if (srcList== srcPokemons && destList==destPokemons){
+                        //sum the grade
                         theGrade += pokemons.get(j).getValue();
                         if (pokemons.get(j).getIsEaten()) {
                             cnt_list.get(i).setWhoEatMe(this);
@@ -160,10 +173,11 @@ public class Agent {
                         else {
                             pokemons.get(j).setIsEaten(true);
                         }}}}
-
+            //list for the short path
             List<NodeData> shortPath = g.shortestPath(this.getSrc(), current);
             double distance = g.getGraph().getNode(current).getWeight()+cnt_list.get(i).getedge().getWeight();
             double calculate = distance/theGrade;
+            //saving the minimum for move the agent to there
             if (calculate < min) {
                 min = calculate;
                 cnt = cnt_list.get(i);
@@ -174,10 +188,12 @@ public class Agent {
         if(indx==-1){
             indx=0;
         }
+        //changing the variables for the eating of the pokemons
         pokemons.get(indx).setIsEaten(true);
         pokemons.get(indx).setWhoEatMe(this);
         this.setPokemon(pokemons.get(indx));
         this.pokemon = pokemons.get(indx);
+        //return the next node for the agents
         return NextNode;
     }
 
