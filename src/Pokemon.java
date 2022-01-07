7fe700025be0a22a4b42710d2ef859afae08fd53
RazElbaz <93310416+RazElbaz@users.noreply.github.com>
2022-01-07 12:42:24 +0200

    Update Pokemon.java

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -16,7 +16,7 @@ public class Pokemon {
     private boolean IsEaten;
 
 
-
+//A contracture that initializes the Pokemon with data
     public Pokemon(GeoLocation po, int ty, double v,DirectedWeightedGraph g) {
         type = ty;
         value = v;
@@ -34,15 +34,17 @@ public class Pokemon {
         }
     }
 
-
+//The function gets a Pokemon position and returns the side it is on
     public static EdgeData findTheEdge(GeoLocation p, int src1, int dest1, DirectedWeightedGraph g) {
         GeoLocation src = g.getNode(src1).getLocation();
         GeoLocation dest = g.getNode(dest1).getLocation();
         boolean ans = false;
         double dist = src.distance(dest);
         double d1 = src.distance(p) + p.distance(dest);
+        //Distance check
         if(dist>d1-EPS) {ans = true;}
         if (ans==true){
+            //Returns the side that the Pokemon is on
             return g.getEdge(src1,dest1);
         }else{
             return null;
@@ -50,7 +52,7 @@ public class Pokemon {
     }
 
     
-
+    //Function that returns EdgeData
     public EdgeData get_edge() {
         return edge;
     }
@@ -58,27 +60,27 @@ public class Pokemon {
     public void set_Id(int e) {
         this.id = e;
     }
-
+    //Function that returns the position
     public GeoLocation getLocation() {
         return pos;
     }
-    
+     //Function that returns the id
     public int getId() {
         return this.id;
     }
-    
+    //Function that returns the value of the pokemon
     public double getValue() {
         return value;
     }
-
+  //Function that change value if the pokemon eaten by an agent
     public void setIsEaten(boolean isIt) {
         this.IsEaten = isIt;
     }
-
+    //Function that returns if the pokemon eaten by an agent
     public boolean getIsEaten() {
         return IsEaten;
     }
-
+    //A function that updates which agent ate the Pokemon
     public void setWhoEatMe(Agent whoEatMe) {this.WhoEatMe = whoEatMe;}
 
 
