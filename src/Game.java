9a27d53bbee9238bfa00919a7508300dd8a09b03
liornagar799 <93442345+liornagar799@users.noreply.github.com>
2022-01-07 12:24:58 +0200

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,63 +1,63 @@
 
 
 public class Game {
+
 private int Num_of_pokemons;
-private boolean is_logged;
 private int moves;
 private int grade;
 private int game_level;
-private int max_level;
 private int id;
-private String graph;
-private int agents;
+private int Num_of_agents;
 
     public Game() {
         this.Num_of_pokemons = -1;
         this.moves = 0;
         this.grade = 0;
         this.id = -1;
-        this.graph = "";
-        this.agents = -1;
+        this.Num_of_agents = -1;
     }
 
     public int getId() {
         return this.id;
     }
+
     public int getNum_of_pokemons() {
         return  this.Num_of_pokemons;
     }
+
     public int getmoves() {
         return this.moves;
     }
-    public String getgraph() {
-        return  this.graph;
-    }
+
     public int getagents() {
-        return  this.agents;
+        return  this.Num_of_agents;
     }
+
     public int getLeavel() {
         return  this.game_level;
     }
 
-    public int getgrade() {
-        return  this.grade;
-    }
+    public int getgrade() {return  this.grade;}
 
+    public void setId(int s) {this.id= s;}
 
-    public void setId(int s) {
-        this.id= s;
-    }
     public void setNum_of_pokemons(int s) {
         this.Num_of_pokemons= s;
     }
+
     public void setmoves(int s) {
         this.moves= s;
     }
-    public void setagents(int s) {this.agents= s;}
+
+    public void setagents(int s) {this.Num_of_agents = s;}
+
     public void setgrade(int s) {
         this.grade= s;
     }
-    public void setgraph(String s) {
-        this.graph= s;
+
+    public void setGame_level(int s) {
+        this.game_level= s;
     }
+
+
 }
